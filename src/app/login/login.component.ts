449a1b9ad3c9c23200c19b6ef0b1e5f0c99b5f2f
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-09-29 10:01:33 +0200

    feat: Implement ReactiveForm with validation and error messages in login component
    - Integrated ReactiveForms in the login component.
    - Added form validation for email and password fields with proper error messages.
    - Displayed error messages for invalid email format and required fields.
    - Included `FormBuilder`, `FormGroup`, and `Validators` for form control.
    - Updated "Remember me" checkbox to be part of the ReactiveForm.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,12 +2,46 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 import { RouterLink } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterLink],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    FooterComponent,
+    RouterLink,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      remember: [false],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+
+      // todo connnect with auth service
+    } else {
+      console.log('invalid form');
+
+      // todo show error message
+    }
+  }
+}
