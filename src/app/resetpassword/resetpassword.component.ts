a8934dfe29448ce141353b3aeb2ae48cc0142c39
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-09-29 10:27:50 +0200

    feat: Add basic layout and ReactiveForm with validation in resetpassword component
    - Implemented initial layout structure for the resetpassword component.
    - Integrated ReactiveForm with form validation for email input.
    - Added error messages for invalid email format and required field.
    - Used `FormBuilder`, `FormGroup`, and `Validators` to manage form controls.
    - Prepared logic for future integration with authentication service.

--- src/app/resetpassword/resetpassword.component.ts
+++ src/app/resetpassword/resetpassword.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/header/header.component';
+import { FooterComponent } from '../shared/footer/footer.component';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-resetpassword',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
   templateUrl: './resetpassword.component.html',
-  styleUrl: './resetpassword.component.scss'
+  styleUrl: './resetpassword.component.scss',
 })
 export class ResetpasswordComponent {
+  resetPasswordForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.resetPasswordForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]], // todo validate email check if exists in db
+    });
+  }
 
+  onSubmit() {
+    console.log(this.resetPasswordForm.value);
+
+    // todo connnect with auth service
+  }
 }
