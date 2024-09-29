04263a80a992cd2bf14f0a93aa0aabfc2bb08a90
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-09-29 10:17:21 +0200

    feat: Implement ReactiveForm with validation and error messages in signup component
    - Integrated ReactiveForms in the signup component.
    - Added form validation for email, password, and confirmPassword fields.
    - Displayed error messages for invalid email format, password length, required fields, and password mismatch.
    - Implemented custom logic to ensure the "Confirm Password" field matches the "Password" field.
    - Included `FormBuilder`, `FormGroup`, and `Validators` for form control.
    - Disabled the "Get Started" button when the form is invalid.

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -2,12 +2,39 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 import { RouterLink } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterLink],
+  imports: [ReactiveFormsModule, HeaderComponent, FooterComponent, RouterLink],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  signupForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.signupForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      console.log(this.signupForm.value);
+
+      // todo connnect with auth service
+    } else {
+      console.log('invalid form');
+
+      // todo show error message
+    }
+  }
+}
