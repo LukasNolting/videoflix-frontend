7c56c737772d15159d60921ec3bad4f5cb991ad5
Lukas Nolting <lukas.nolting@icloud.com>
2024-09-29 13:02:40 +0200

    landingsite

--- src/app/landingsite/landingsite.component.ts
+++ src/app/landingsite/landingsite.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/header/header.component';
+import { FooterComponent } from '../shared/footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-landingsite',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, RouterLink],
   templateUrl: './landingsite.component.html',
-  styleUrl: './landingsite.component.scss'
+  styleUrl: './landingsite.component.scss',
 })
-export class LandingsiteComponent {
-
-
-}
+export class LandingsiteComponent {}
